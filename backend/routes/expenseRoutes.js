3895bde29f0ff1d241fb2a0605126d419e522b7d
AnushaShetty57 <m.anushashetty26@gmail.com>
2026-03-18 09:57:45 +0530

    Expense backend completed

--- backend/routes/expenseRoutes.js
+++ backend/routes/expenseRoutes.js
@@ -1,8 +1,16 @@
 const express = require("express");
 const router = express.Router();
 
-const { addExpense } = require("../controllers/expenseController");
+const {
+ addExpense,
+ getGroupExpenses,
+ calculateBalances,
+ getExpenseDetails
+} = require("../controllers/expenseController");
 
 router.post("/add-expense", addExpense);
+router.get("/group/:groupId", getGroupExpenses);
+router.get("/balances/:groupId", calculateBalances);
+router.get("/:expenseId", getExpenseDetails);
 
 module.exports = router;
