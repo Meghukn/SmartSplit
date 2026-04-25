53a6e0c98e84f09841e555f6a4405399f33c1cbc
Meghana <meghukn16@gmail.com>
2026-04-25 10:06:30 +0530

    updated

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,7 @@ app.use(cors({
   origin:
     process.env.NODE_ENV === "production"
       ? process.env.FRONTEND_URL
-      : "http://localhost:5137",
+      : "http://localhost:5173",
   credentials: true
 }));
 app.use(express.json());
